ed306f63282366599c827db8f3e499e400850429
hardikchaudhry2608 <141809452+hardikchaudhry2608@users.noreply.github.com>
2025-02-11 11:01:23 +0000

    datatypes

---
+++ _01_basic/02_datatype.js
@@ -0,0 +1,22 @@
+
+
+// Premitive data type 
+
+// Number
+// BigInt
+// Boolean
+// string
+// null => standalone value
+// undefined
+// symbol => unique 
+// float
+
+
+
+// Object 
+
+// console.log(typeof "hardik"); => string
+// console.log(typeof null);  => object
+// console.log(typeof undefined); => undefined
+
+
